e8f1bca71b4f66a4bc8d5f134a45a81cb02f49a8
iamthesandeepsuthar <sandeep.suthar08@gmail.com>
2021-09-27 01:15:27 +0530

    Db Table

---
+++ AurigainLoanERP/AurigainLoanERP.Database/UserKYC.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[UserKYC]
+(
+[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY,    
+	[KYCNumber] NVARCHAR(200) NOT NULL, 
+    [KYCDocumentTypeId] INT NOT NULL references DocumentType(Id), 
+    [UserId] bigint references UserMaster(Id) NOT NULL,
+     [IsActive] BIT NOT NULL DEFAULT 1, 
+    [IsDelete] BIT NOT NULL DEFAULT 0, 
+    [CreatedOn] DATETIME NOT NULL DEFAULT GETDATE(), 
+    [ModifiedOn] DATETIME NULL, 
+    [CreatedBy] BIGINT NULL, 
+    [ModifiedBy] BIGINT NULL, 
+)
