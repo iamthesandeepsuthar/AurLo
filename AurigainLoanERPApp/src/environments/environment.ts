02d3c69e33d44674975b83d8a570618e232e4e49
aakash-agrawal14 <86488872+aakash-agrawal14@users.noreply.github.com>
2021-10-01 22:19:20 +0530

    Commit: Chnage Base Api Path

--- AurigainLoanERPApp/src/environments/environment.ts
+++ AurigainLoanERPApp/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   AESKey: "8x/A?D(G+KaPdSgVkYp3s6v9y$B&E)H@",
-  apiEndPoint: "http://localhost:55017/api/",
+  apiEndPoint: "http://localhost:62741/api/",
   IsAutoLogin: true
 };
 
