b53a4b59a9607407018bbee18a42cd41ceaf439d
aakash-agrawal14 <86488872+aakash-agrawal14@users.noreply.github.com>
2021-10-05 22:23:20 +0530

    Commit: Add Nominee Relationship Eunm at angular project

--- AurigainLoanERPApp/src/app/Shared/Model/app-enums.ts
+++ AurigainLoanERPApp/src/app/Shared/Model/app-enums.ts
@@ -1,2 +1,15 @@
 export enum AppEnums {
 }
+export enum NomineeRelationshipEnum{
+            Mother = 'Mother',
+            Father ='Father',
+            Son = 'Son',
+            Brother = 'Brother',
+            Sister = 'Sister',
+            Wife = 'Wife',
+            Husband = 'Husband',
+            GrandFather = 'GrandFather',
+            GrandMother = 'GrandMother',
+            Daughter= 'Daughter'
+}
+
