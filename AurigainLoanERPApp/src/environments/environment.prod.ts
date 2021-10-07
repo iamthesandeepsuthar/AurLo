5f145367466c53e3a609df1a682cd1004e9236ba
aakash-agrawal14 <86488872+aakash-agrawal14@users.noreply.github.com>
2021-10-07 22:32:07 +0530

    Commit: Add Toster on user role

--- AurigainLoanERPApp/src/environments/environment.prod.ts
+++ AurigainLoanERPApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   AESKey:"8x/A?D(G+KaPdSgVkYp3s6v9y$B&E)H@",
-  apiEndPoint :"http://localhost:55017/api/",
+  apiEndPoint :"http://localhost:62741/api/",
   IsAutoLogin: true
 };
