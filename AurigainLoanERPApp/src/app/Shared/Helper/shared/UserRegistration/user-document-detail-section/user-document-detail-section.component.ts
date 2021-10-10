d782ec7ca74f893b40e2480d233e9762b148ce3c
iamthesandeepsuthar <sandeep.suthar08@gmail.com>
2021-10-10 15:51:00 +0530

    Changes

--- AurigainLoanERPApp/src/app/Shared/Helper/shared/UserRegistration/user-document-detail-section/user-document-detail-section.component.ts
+++ AurigainLoanERPApp/src/app/Shared/Helper/shared/UserRegistration/user-document-detail-section/user-document-detail-section.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FileInfo } from 'src/app/Content/Common/file-selector/file-selector.component';
 import { DocumentPostModel } from 'src/app/Shared/Model/doorstep-agent-model/door-step-agent.model';
+import { FilePostModel } from '../../../../Model/doorstep-agent-model/door-step-agent.model';
 
 @Component({
   selector: 'app-user-document-detail-section',
@@ -23,5 +24,16 @@ export class UserDocumentDetailSectionComponent implements OnInit {
   }
   onFrmSubmit() {
     this.onSubmit.emit(this.documentModel);
+  }
+  onDocumentAttach(docuemtnTypeId: number, file: FileInfo, isEdit: boolean) {
+    debugger;
+    let doc  = {} as DocumentPostModel;
+    doc.Files = [] as FilePostModel[];
+    doc.DocumentTypeId = 1;
+    doc.Files[0].File = file.FileBase64;
+    doc.Files[0].FileName = file.Name;
+    doc.Files[0].IsEditMode = false;
+
+
   }
 }
