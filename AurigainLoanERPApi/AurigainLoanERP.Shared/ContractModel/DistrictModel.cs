f1a30ad3970d3a17cd211fc654d60ccda4d4adb7
aakash-agrawal14 <86488872+aakash-agrawal14@users.noreply.github.com>
2021-10-23 23:50:23 +0530

    Commit : changes

--- AurigainLoanERPApi/AurigainLoanERP.Shared/ContractModel/DistrictModel.cs
+++ AurigainLoanERPApi/AurigainLoanERP.Shared/ContractModel/DistrictModel.cs
@@ -15,8 +15,7 @@ namespace AurigainLoanERP.Shared.ContractModel
         }
         public long Id { get; set; }
         public string Name { get; set; }
-        public int StateId { get; set; }
-        public string Pincode { get; set; }
+        public int StateId { get; set; }       
         public string StateName { get; set;}
         public bool? IsActive { get; set; }
         public bool IsDelete { get; set; }
--- AurigainLoanERPApp/src/app/Shared/Model/master-model/jewellary-type-model.model.ts
+++ AurigainLoanERPApp/src/app/Shared/Model/master-model/jewellary-type-model.model.ts
@@ -1,3 +1,16 @@
+
 export class JewellaryTypeModel {
-  
+  Id!: number;
+  Name!: string;
+  Description!: string;
+  IsActive!: boolean | null;
+  IsDelete!: boolean | null;
+  CreatedOn!: string;
+  ModifiedOn!: string | null;
+  CreatedBy!: number | null;
+}
+
+export class DDLJewellaryType {
+  Name!: string;
+  Id!: number;
 }
