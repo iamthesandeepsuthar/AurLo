53eb624079dd8d71f3ac39e9296d32ff5a32a11b
aakash-agrawal14 <86488872+aakash-agrawal14@users.noreply.github.com>
2021-10-24 13:24:03 +0530

    Commit: Add and changes in api and model

--- AurigainLoanERPApi/AurigainLoanERP.Services/Bank/IBranchService.cs
+++ AurigainLoanERPApi/AurigainLoanERP.Services/Bank/IBranchService.cs
@@ -10,7 +10,7 @@ namespace AurigainLoanERP.Services.Bank
     public interface IBranchService
     {
         Task<ApiServiceResponseModel<BranchModel>> GetById(int id);
-        Task<ApiServiceResponseModel<List<DDLBrnachModel>>> Branches(int bankId);
+        Task<ApiServiceResponseModel<List<DDLBranchModel>>> Branches(int bankId);
         Task<ApiServiceResponseModel<string>> AddUpdateAsync(BranchModel model);
         Task<ApiServiceResponseModel<object>> UpateActiveStatus(int id);
         Task<ApiServiceResponseModel<object>> UpdateDeleteStatus(int id);
--- AurigainLoanERPApp/src/app/Shared/Model/master-model/product-model.model.ts
+++ AurigainLoanERPApp/src/app/Shared/Model/master-model/product-model.model.ts
@@ -1,2 +1,25 @@
+import { ProductCategoryModel } from "./product-category-model.model";
+
 export class ProductModel {
+  Id!: number;
+  Name!: string;
+  Notes!: string;
+  ProductCategoryId!: number;
+  MinimumAmount!: number | null;
+  MaximumAmount!: number | null;
+  InterestRate!: number | null;
+  ProcessingFee!: number | null;
+  InterestRateApplied!: boolean | null;
+  MinimumTenure!: number | null;
+  MaximumTenure!: number | null;
+  IsActive!: boolean;
+  IsDelete!: boolean;
+  CreatedDate!: string;
+  CreatedBy!: number | null;
+  Category!: ProductCategoryModel;
+}
+
+export class DDLProductModel {
+  Id!: number;
+  Name!: string;
 }
