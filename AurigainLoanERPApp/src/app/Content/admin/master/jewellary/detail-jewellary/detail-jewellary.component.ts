323d9992947ef4326273fbde30884fd07ce49bf7
aakash-agrawal14 <86488872+aakash-agrawal14@users.noreply.github.com>
2021-10-29 11:42:06 +0530

    Commit: add door step availablity list  and changes in master pages

--- AurigainLoanERPApp/src/app/Content/admin/master/jewellary/detail-jewellary/detail-jewellary.component.ts
+++ AurigainLoanERPApp/src/app/Content/admin/master/jewellary/detail-jewellary/detail-jewellary.component.ts
@@ -1,15 +1,41 @@
+import { JewellaryTypeModel } from './../../../../../Shared/Model/master-model/jewellary-type-model.model';
+import { JewelleryTypeService } from './../../../../../Shared/Services/master-services/jewellery-type.service';
 import { Component, OnInit } from '@angular/core';
+import { Routing_Url } from 'src/app/Shared/Helper/constants';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-detail-jewellary',
   templateUrl: './detail-jewellary.component.html',
-  styleUrls: ['./detail-jewellary.component.scss']
+  styleUrls: ['./detail-jewellary.component.scss'],
+  providers: [JewelleryTypeService]
 })
 export class DetailJewellaryComponent implements OnInit {
+  Id: number = 0;
+  model = new JewellaryTypeModel();
+  get routing_Url() { return Routing_Url }
+  get Model(): JewellaryTypeModel{  return this.model; }
 
-  constructor() { }
-
+  constructor(private readonly _typeService: JewelleryTypeService,
+              private _activatedRoute: ActivatedRoute ,
+              private readonly toast: ToastrService) {
+    if (this._activatedRoute.snapshot.params.id) {
+      this.Id = this._activatedRoute.snapshot.params.id;
+    }
+  }
   ngOnInit(): void {
+    this.onGetDetail();
+  }
+  onGetDetail() {
+    let subscription = this._typeService.JewelleryTypeById (this.Id).subscribe(res => {
+      subscription.unsubscribe();
+      if (res.IsSuccess) {
+      this.model = res.Data as JewellaryTypeModel;
+      } else {
+    this.toast.warning('Record not found', 'No Record');
+      }
+    });
   }
 
 }
