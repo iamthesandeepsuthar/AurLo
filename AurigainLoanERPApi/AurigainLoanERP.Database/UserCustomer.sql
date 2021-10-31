1c31d14b1ff83e2663e5b05f2295033bf791154e
aakash-agrawal14 <86488872+aakash-agrawal14@users.noreply.github.com>
2021-10-31 11:10:06 +0530

    Commit: add customer table in database project

---
+++ AurigainLoanERPApi/AurigainLoanERP.Database/UserCustomer.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[UserCustomer]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY Identity, 
+    [FullName] NVARCHAR(2000) NULL, 
+    [FatherName] NVARCHAR(2000) NULL, 
+    [Gender] NVARCHAR(30) NULL,  
+    [UserId] bigint references UserMaster(Id) not null,
+    [PincodeAreaId] BIGINT references PincodeArea(Id),
+    [Address] NVARCHAR(MAX) NULL,        
+    [DateOfBirth] DateTime,       
+    [IsActive] BIT NOT NULL DEFAULT 1, 
+    [IsDelete] BIT NOT NULL DEFAULT 0, 
+    [CreatedOn] DATETIME NOT NULL DEFAULT GETDATE(), 
+    [ModifiedOn] DATETIME NULL, 
+    [CreatedBy] BIGINT NULL, 
+    [ModifiedBy] BIGINT NULL, 
+)
