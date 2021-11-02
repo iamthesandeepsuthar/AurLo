220a4b1361fd446bc6ad47dc8d82fd33302646c9
sandeep.suthar@dotsqaures.com-at-180283404574 <sandeep.suthar@dotsquares.com>
2021-11-02 11:42:50 +0530

    chnges

--- AurigainLoanERPApp/src/app/Content/Common/navigation/navigation.component.ts
+++ AurigainLoanERPApp/src/app/Content/Common/navigation/navigation.component.ts
@@ -13,8 +13,8 @@ export class NavigationComponent implements OnInit {
   get routing_Url() { return Routing_Url }
   roleEnum = UserRoleEnum;
   userModel = {} as LoginResponseModel;
-  IsAdminMenu!: boolean;
-  IsCustomerMenu!: boolean;
+  IsAdminMenu: boolean=false;
+  IsCustomerMenu: boolean=false;
   constructor(private readonly _authService: AuthService) {
     this.IsAdminMenu = false;
     this.IsCustomerMenu = false;
@@ -22,13 +22,14 @@ export class NavigationComponent implements OnInit {
 
   ngOnInit(): void {
     this._authService.IsAuthentication.subscribe(x => {
+      this._authService.IsAuthenticate();
       this.userModel = this._authService.GetUserDetail() as LoginResponseModel;
       if(this.userModel.RoleId == this.roleEnum.Customer) {
-        this.IsAdminMenu = false;
+      
         this.IsCustomerMenu = true;
       } else {
         this.IsAdminMenu = true;
-        this.IsCustomerMenu = false;
+      
       }
     });
   }
