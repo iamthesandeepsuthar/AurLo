c0f2daa3e442e82edc3cd10a9ab90c3c5bd9ba3a
iamthesandeepsuthar <sandeep.suthar08@gmail.com>
2021-11-14 23:18:44 +0530

    changes

--- AurigainLoanERPApp/src/app/Content/user-customer/Leads/gold-loan-fresh-lead/add-edit-gold-loan-fresh-lead/add-edit-gold-loan-fresh-lead.component.ts
+++ AurigainLoanERPApp/src/app/Content/user-customer/Leads/gold-loan-fresh-lead/add-edit-gold-loan-fresh-lead/add-edit-gold-loan-fresh-lead.component.ts
@@ -8,37 +8,62 @@ import { GoldLoanFreshLeadModel } from 'src/app/Shared/Model/Leads/gold-loan-fre
   styleUrls: ['./add-edit-gold-loan-fresh-lead.component.scss']
 })
 export class AddEditGoldLoanFreshLeadComponent implements OnInit {
-  model = {} as GoldLoanFreshLeadModel;
-  registrationFromStepOne!: FormGroup;
-  registrationFromStepTwo!: FormGroup;
-  registrationFromStepThird!: FormGroup;
-  constructor(private readonly fb: FormBuilder) { }
+  model = new GoldLoanFreshLeadModel();
+  
+  leadFromPersonalDetail!: FormGroup;
+  leadFromDocumentDetail!: FormGroup;
+  leadFromJewelleryDetail!: FormGroup;
+  leadFromAppointmentDetail!: FormGroup;
+
+  constructor(private readonly fb: FormBuilder) {
+  }
 
   ngOnInit(): void {
   }
 
   formInit() {
-    this.registrationFromStepOne = this.fb.group({
-      fullName: [undefined, Validators.required],
-      fatherName: [undefined, Validators.required],
-      email: [undefined, Validators.required],
-      mobile: [undefined, Validators.required],
-      gender: [undefined, undefined],
-      dob: [undefined,undefined],
-
-    });
-    this.registrationFromStepTwo = this.fb.group({
-      documentType: [undefined],
-      documentNumber: [undefined],
-    });
-    this.registrationFromStepThird = this.fb.group({
-      pincode: [undefined],
-      address: [undefined],
-      area: [undefined, Validators.required],
+    this.leadFromPersonalDetail = this.fb.group({
+      Product: [undefined, Validators.required],
+      Email: [undefined, Validators.required],
+      FullName: [undefined, Validators.required],
+      FatherName: [undefined, Validators.required],
+      DOB: [undefined, Validators.required],
+      Gender: [undefined, undefined],
+      Mobile: [undefined, Validators.required],
+      AlternativeMobile: [undefined, Validators.required],
+      Amount: [undefined, Validators.required],
+      Purpose: [undefined, Validators.required]
     });
 
+    this.leadFromDocumentDetail = this.fb.group({
+      DocumentType: [undefined],
+      DocumentNumber: [undefined],
+      Pincode: [undefined],
+      Aarea: [undefined, Validators.required],
+    });
+
+    this.leadFromJewelleryDetail = this.fb.group({
+      JewelleryType: [undefined, Validators.required],
+      Quantity: [undefined, Validators.required],
+      Weight: [undefined, Validators.required],
+      Karats: [undefined, Validators.required],
+      Tenure: [undefined, Validators.required]
+    });
+
+    this.leadFromAppointmentDetail = this.fb.group({
+      Branch: [undefined, Validators.required],
+      DateofAppointment: [undefined, Validators.required],
+      TimeofAppointment: [undefined, Validators.required],
+    });
 
 
+
+  }
+  onSubmit() {
+    this.leadFromPersonalDetail.markAllAsTouched();
+    this.leadFromDocumentDetail.markAllAsTouched();
+    this.leadFromJewelleryDetail.markAllAsTouched();
+    this.leadFromAppointmentDetail.markAllAsTouched();
   }
 
 }
