242679e0acd3136a3865cc07bd1475e8e02e09b9
iamthesandeepsuthar <sandeep.suthar08@gmail.com>
2021-11-20 23:15:27 +0530

    Changes in Table

---
+++ AurigainLoanERPApi/AurigainLoanERP.Database/BTGoldLoanLeadActionHistory.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[BTGoldLoanLeadActionHistory]
+(
+	[Id] BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [LeadId] BIGINT NOT NULL  references BTGoldLoanLead(Id),
+	[LeadStatus] int null,
+	[AssignFromUserId] bigint references UserMaster(Id),
+	[AssignToUserId] bigint references UserMaster(Id),
+	[ActionDate] DateTime not null default getdate()
+)
