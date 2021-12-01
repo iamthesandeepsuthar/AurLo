ee2aa56c54047268356cbc0ac105354ea501ad32
iamthesandeepsuthar <sandeep.suthar08@gmail.com>
2021-12-01 23:41:48 +0530

    Changes

--- AurigainLoanERPApp/src/app/Content/Common/header/header.component.ts
+++ AurigainLoanERPApp/src/app/Content/Common/header/header.component.ts
@@ -1,6 +1,8 @@
 import { LoginResponseModel } from 'src/app/Shared/Model/User-setting-model/user-setting.model';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AuthService } from '../../../Shared/Helper/auth.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ChangePasswordDialogComponent } from '../../../Shared/Helper/shared/Dialogs/change-password-dialog/change-password-dialog.component';
 
 @Component({
   selector: 'app-header',
@@ -10,7 +12,7 @@ import { AuthService } from '../../../Shared/Helper/auth.service';
 export class HeaderComponent implements OnInit {
   userLoginModel = {} as LoginResponseModel;
   @Output() SetTheme = new EventEmitter<string>();
-  constructor(readonly _authService: AuthService) { }
+  constructor(readonly _authService: AuthService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     setTimeout(() => {
@@ -22,6 +24,18 @@ export class HeaderComponent implements OnInit {
 
   changeTheme(themeName: string = '') {
     this.SetTheme.emit(themeName);
+  }
+
+  openChangePasswordDialog(): void {
+    const dialogRef = this.dialog.open(ChangePasswordDialogComponent, {
+      width: '250px',
+
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+
+
+    });
   }
 }
 
