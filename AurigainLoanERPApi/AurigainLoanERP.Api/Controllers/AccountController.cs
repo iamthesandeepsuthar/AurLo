df14d373e4ab04d156b655d6876500d50df5e5eb
iamthesandeepsuthar <sandeep.suthar08@gmail.com>
2021-12-07 13:17:06 +0530

    Changes

--- AurigainLoanERPApi/AurigainLoanERP.Api/Controllers/AccountController.cs
+++ AurigainLoanERPApi/AurigainLoanERP.Api/Controllers/AccountController.cs
@@ -23,43 +23,50 @@ namespace AurigainLoanERP.Api.Controllers
         public async Task<ApiServiceResponseModel<LoginResponseModel>> WebLogin(LoginModel model)
         {
             return await _accountService.WebLogin(model);           
-        }        
+        }
+        
         //Post api/Account/GetOTP
         [HttpPost]
         public async Task<ApiServiceResponseModel<OtpModel>> GetOTP(OtpRequestModel model)
         {
             return await _accountService.GetOtp(model);
         }
+
         //Post api/Account/WebChangePassword
         [HttpPost]
         public async Task<ApiServiceResponseModel<string>>WebChangePassword(ChangePasswordModel model)
         {
             return await _accountService.WebChangePassword(model);
         }
+
         //Post api/Account/ChangeMPIN
         [HttpPost]
         public async Task<ApiServiceResponseModel<string>> ChangeMPIN(ChangePasswordModel model)
         {
             return await _accountService.ChangePassword(model);
         }
+
         //Post api/Account/Login
         [HttpPost]
         public async Task<ApiServiceResponseModel<LoginResponseModel>> Login(LoginModel model)
         {
             return await _accountService.Login(model);
         }
+
         //Post api/Account/VarifiedMPIN
         [HttpPost]
         public async Task<ApiServiceResponseModel<string>> VarifiedMPIN(OtpVerifiedModel model)
         {
             return await _accountService.VerifiedPin(model);
         }
+
         [HttpGet]
         //Get api/Account/ValidateUserWithMobileNUmber
         public async Task<ApiServiceResponseModel<string>> ValidateUserWithMobileNUmber(string mobileNumber) 
         {
             return await _accountService.CheckUserExist(mobileNumber);
         }
+
         [HttpGet("{value}")]
         public ApiServiceResponseModel<string> GenerateEncrptPassword(string value)
         {
