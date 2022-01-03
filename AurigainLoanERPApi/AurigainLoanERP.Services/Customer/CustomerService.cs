c859b5d4d50c30514dc6499698453a96a73563d9
iamthesandeepsuthar <sandeep.suthar08@gmail.com>
2022-01-04 01:28:49 +0530

    chnges

--- AurigainLoanERPApi/AurigainLoanERP.Services/Customer/CustomerService.cs
+++ AurigainLoanERPApi/AurigainLoanERP.Services/Customer/CustomerService.cs
@@ -274,11 +274,11 @@ namespace AurigainLoanERP.Services.Customer
                         Mobile = detail.User.Mobile,
                         Gender = detail.Gender,
                         DateOfBirth = detail.DateOfBirth,
-                        Address = detail.Address,
-                        AreaName = detail.PincodeArea.AreaName,
-                        Pincode = detail.PincodeArea.Pincode,
-                        District = detail.PincodeArea.District.Name,
-                        State = detail.PincodeArea.District.State.Name,
+                        Address = detail.Address?? null,
+                        AreaName = detail.PincodeArea != null? detail.PincodeArea.AreaName:null ,
+                        Pincode = detail.PincodeArea!=null? detail.PincodeArea.Pincode:null ,
+                        District = detail.PincodeArea!=null? detail.PincodeArea.District.Name:null ,
+                        State = detail.PincodeArea!=null? detail.PincodeArea.District.State.Name:null ,
                         IsActive = detail.IsActive,
                         CreatedOn = detail.CreatedOn,
 
@@ -292,7 +292,7 @@ namespace AurigainLoanERP.Services.Customer
                             Id = doc.Id,
                             KycdocumentTypeId = doc.KycdocumentTypeId,
                             Kycnumber = doc.Kycnumber,
-                            KycdocumentTypeName = doc.KycdocumentType.DocumentName,
+                            KycdocumentTypeName = doc.KycdocumentType!=null? doc.KycdocumentType.DocumentName:null,
                             UserId = doc.UserId
                         };
                         customer.KycDocuments.Add(kycDoc);
---
+++ AurigainLoanERPApp/src/app/Shared/Helper/shared/Directives/title-case.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[TitleCase]'
+})
+export class TitleCaseDirective {
+
+
+  lastValue!: string;
+
+  constructor(public ref: ElementRef) { }
+
+  @HostListener('input', ['$event']) onInput($event: any) {
+    let str = $event.target.value;
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    str = str.join(' ');
+    $event.preventDefault();
+    if (!this.lastValue || (this.lastValue && str.length > 0 && this.lastValue !== str)) {
+      this.lastValue = this.ref.nativeElement.value = str;
+      // Propagation
+      const evt = document.createEvent('HTMLEvents');
+      evt.initEvent('input', false, true);
+      $event.target.dispatchEvent(evt);
+    }
+  }
+}
